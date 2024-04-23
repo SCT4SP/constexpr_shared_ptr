bc471008d6a61ab42a9882cbabc1245f0725825d
Paul Keir <pkeir@outlook.com>
2024-04-23 16:18:58 +0100

    constexpr branch ok with 14.0.1

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MYINCLUDE="../include/c++/14.0.0"
+MYINCLUDE="../include/c++/14.0.1"
 
 MYGCC="/opt/gcc-latest/bin/g++ -Wl,-rpath,"/opt/gcc-latest/lib64:$LD_LIBRARY_PATH""
 MYGCCFLAGS="-g -std=c++26 -Winvalid-constexpr -fsanitize=address -static-libasan -I ${MYINCLUDE} -I ${MYINCLUDE}/x86_64-pc-linux-gnu"
